d29a763cc069c853878467238fc3346ffccb55e4
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2022-07-30 13:23:40 +0200

    Send all files

---
+++ canvas.js
@@ -0,0 +1,147 @@
+let canvas = document.getElementById('canvas');
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer({
+	canvas: canvas
+});
+/* CAMERA POSITION */
+camera.position.z = 1.8
+camera.position.x = 0
+camera.position.y = 0.3
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshStandardMaterial({
+	color: 'white',
+	roughness: 0.4,
+	metalness: 0.6
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+const spotLight = new THREE.SpotLight('lightgrey');
+spotLight.position.set(-1.3, 1.1, 1);
+scene.add(spotLight);
+renderer.setClearColor(0xffffff, 0)
+cube.rotation.x = -0.5
+
+const torusGeometry = new THREE.SphereGeometry(0.1, 32, 16);
+const redMaterial = new THREE.MeshStandardMaterial({
+	color: 'red',
+	roughness: 0.5,
+	metalness: 0.6
+});
+const torusMaterial = new THREE.MeshStandardMaterial({
+	color: 'white',
+	roughness: 0.5,
+	metalness: 0.6
+});
+/* MAKE A DICE */
+const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus2 = new THREE.Mesh(torusGeometry, redMaterial);
+const torus3 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus4 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus5 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus6 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus7 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus8 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus9 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus10 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus11 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus12 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus13 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus14 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus15 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus16 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus17 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus18 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus19 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus20 = new THREE.Mesh(torusGeometry, torusMaterial);
+const torus21 = new THREE.Mesh(torusGeometry, torusMaterial);
+
+torus.position.set(0.45, 0, 0) //red middlle +3
+torus2.position.set(0, -0.45, 0) //green -1
+torus3.position.set(0, 0, 0.45) //
+torus4.position.set(0.3, 0.3, -0.45) //blue top -2
+torus5.position.set(0.45, 0.3, 0.3) //red top +3
+torus6.position.set(0.45, -0.3, -0.3) // red bottom +3
+torus7.position.set(0.3, 0.3, 0.45) //blue top right +5
+torus8.position.set(-0.3, -0.3, 0.45) //blue bottom left +5
+torus9.position.set(0.3, -0.3, 0.45) //blue bottom right +5
+torus10.position.set(-0.3, 0.3, 0.45) //blue top left +5
+torus11.position.set(0.3, 0.45, 0.3) //green +6
+torus12.position.set(0.3, 0.45, -0.3) // green +6
+torus13.position.set(0.3, 0.45, 0) // green +6
+torus14.position.set(-0.3, 0.45, 0.3) //green +6
+torus15.position.set(-0.3, 0.45, -0.3) //green +6
+torus16.position.set(-0.3, 0.45, 0) //green +6
+torus17.position.set(-0.3, -0.3, -0.45) //blue bottom -2
+torus18.position.set(-0.45, 0.3, 0.3) // red -4
+torus19.position.set(-0.45, 0.3, -0.3) // red -4
+torus20.position.set(-0.45, -0.3, 0.3) // red -4
+torus21.position.set(-0.45, -0.3, -0.3) // red -4
+
+const group = new THREE.Group();
+group.add(torus);
+group.add(torus2);
+group.add(torus3);
+group.add(torus4);
+group.add(torus5);
+group.add(torus6);
+group.add(torus7);
+group.add(torus8);
+group.add(torus9);
+group.add(torus10);
+group.add(torus11);
+group.add(torus12);
+group.add(torus13);
+group.add(torus14);
+group.add(torus15);
+group.add(torus16);
+group.add(torus17);
+group.add(torus18);
+group.add(torus19);
+group.add(torus20);
+group.add(torus21);
+cube.add(group);
+
+function time() {
+	setTimeout(() => {
+		cancelAnimationFrame(id)
+	}, 987);
+}
+
+function animate() {
+	id = requestAnimationFrame(animate)
+	cube.rotation.x += 6 * Math.PI / 180;
+	renderer.render(scene, camera);
+	time()
+};
+
+function jeux() {
+	switch (aleatoire) {
+		case 1:
+			animate()
+			cube.rotation.x = 4.3
+			break;
+		case 2:
+			animate()
+			cube.rotation.x = 2.7
+			break;
+		case 3:
+			animate()
+			cube.rotation.y = 4.7
+			break;
+		case 4:
+			animate()
+			cube.rotation.y = 1.6
+			break;
+		case 5:
+			animate()
+			cube.rotation.y = 6.3
+			break;
+		case 6:
+			animate()
+			cube.rotation.x = 1
+			break;
+	}
+}
+renderer.render(scene, camera);
---
+++ script.js
@@ -0,0 +1,265 @@
+let button = document.getElementById('bouton')
+let hold = document.getElementById('hold')
+let total1 = document.getElementById('total1')
+let total2 = document.getElementById('total2')
+let tour = document.getElementById('tour')
+let newGame = document.getElementById('newGame')
+let online = document.getElementById('online')
+let online2 = document.getElementById('online2')
+let inputPlayer1 = document.getElementById('inputPlayer1')
+let inputPlayer2 = document.getElementById('inputPlayer2')
+let buzzer = document.getElementById('buzzer')
+let holdMp3 = document.getElementById('holdMp3')
+let play = document.getElementById('play')
+let rules = document.getElementById('rules')
+let modalRules = document.getElementById('modalRules')
+let closeBtn2 = document.getElementById('closeBtn2')
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
+let modal = document.getElementById('simpleModal');
+let sound = document.getElementById('sound')
+let winner = document.getElementById('winner')
+let modalWin = document.getElementById('modalWin')
+let buttonModalWin = document.getElementById('buttonModalWin')
+let apllaudissement = document.getElementById('apllaudissement')
+
+//SET RANDOM NUMBERS
+function getRandomInt(max) {
+	return Math.ceil(Math.random() * max);
+}
+
+class Player {
+	constructor(name, actif, roundPoints, globalPoints) {
+		this.name = name
+		this.actif = actif
+		this.roundPoints = 0
+		this.globalPoints = 0
+	}
+}
+
+let player1 = new Player('joueur1', true)
+let player2 = new Player('Joueur2', false)
+
+//ajoute points du dés et supprime points si 1
+function addPointPlayer() {
+	aleatoire = getRandomInt(6)
+	setTimeout(() => {
+		if (player1.actif)
+			if (aleatoire === 1) {
+				buzzer.play()
+				player1.roundPoints = 0
+				totalCurrent1.innerText = player1.roundPoints
+				player1.actif = false
+				player2.actif = true
+				online.style.display = "none"
+				online2.style.display = "block"
+			} else {
+				player1.roundPoints += aleatoire
+				totalCurrent1.innerText = player1.roundPoints
+			}
+		else {
+			if (aleatoire === 1) {
+				buzzer.play()
+				player2.roundPoints = 0
+				totalCurrent2.innerText = player2.roundPoints
+				player1.actif = true
+				player2.actif = false
+				online2.style.display = "none"
+				online.style.display = "block"
+			} else {
+				player2.roundPoints += aleatoire
+				totalCurrent2.innerText = player2.roundPoints
+			}
+		}
+	}, 1200);
+}
+// SET IDENTITY AND SCORE PLAYERS
+function sayIdentite() {
+	total1.innerText = player1.globalPoints
+	totalCurrent1.innerText = player1.roundPoints
+	total2.innerText = player2.globalPoints
+	totalCurrent2.innerText = player2.roundPoints
+}
+// CHANGE POINT IN GLOBAL POINT AND TOGLE PLAYER
+function addPointTOGlobalPoint() {
+	if (player1.actif) {
+		player1.globalPoints += player1.roundPoints
+		player1.roundPoints = 0
+		player1.actif = false
+	} else {
+		player2.globalPoints += player2.roundPoints
+		player2.roundPoints = 0
+		player1.actif = true
+	}
+}
+// CHECKED PLAYERS POINTS AFTER HOLDING
+function verif() {
+	if (player1.globalPoints >= 100) {
+		openmodalWin()
+		sayWinnerName1()
+		apllaudissement.play()
+		reset()
+	}
+
+	if (player2.globalPoints >= 100) {
+		openmodalWin()
+		sayWinnerName2()
+		apllaudissement.play()
+		reset()
+	}
+}
+// RESET THE GAME
+function reset() {
+	player1.globalPoints = 0
+	player1.roundPoints = 0
+	player2.roundPoints = 0
+	player2.globalPoints = 0
+	player1.actif = true
+	sayIdentite()
+}
+// REMOVE PLAYER  
+function removeOnline() {
+	if (player1.actif === false) {
+		online.style.display = "none"
+		online2.style.display = "block"
+	} else {
+		online2.style.display = "none"
+		online.style.display = "block"
+	}
+}
+// SET DICE INITIAL POSITION
+function position() {
+	cube.rotation.x = -0.6
+	cube.rotation.y = 0
+}
+/* SET TIME AFTER CLICK */
+function debounce(callback, delay) {
+	let timer
+	return function() {
+		const args = arguments
+		const context = this
+		clearTimeout(timer)
+		timer = setTimeout(function() {
+			callback.apply(context, args)
+		}, delay)
+	}
+}
+// INSERT PLAYER NAME
+function insertName() {
+	namePlayer1.innerText = inputPlayer1.value
+	namePlayer2.innerText = inputPlayer2.value
+}
+// OPEN MODAL
+function openModal() {
+	modal.style.display = 'block';
+}
+
+function openModal2() {
+	modalRules.style.display = 'block'
+}
+
+function openmodalWin() {
+	modalWin.style.display = 'block'
+}
+// CLOSE MODAL
+function closeModal() {
+	modal.style.display = 'none';
+}
+
+function closeModal2() {
+	modalRules.style.display = 'none'
+}
+
+function closeModalWin() {
+	modalWin.style.display = 'none'
+}
+
+// Click outside and close
+function outsideClick(e) {
+	if (e.target == modal) {
+		modal.style.display = 'none';
+	}
+}
+
+function outsideClick2(e) {
+	if (e.target == modalRules) {
+		modalRules.style.display = 'none';
+	}
+}
+
+function outsideClickModalWin(e) {
+	if (e.target == modalWin) {
+		modalWin.style.display = 'none';
+	}
+}
+// Listen button new-game
+newGame.addEventListener('click', () => {
+	reset()
+	removeOnline()
+})
+// Listen button Roll dice
+button.addEventListener('click', debounce(function(e) {
+	addPointPlayer()
+	sayIdentite()
+	removeOnline()
+	position()
+	jeux()
+	verif()
+	sound.play()
+}, 1000))
+// modal new game
+play.addEventListener('click', () => {
+	insertName()
+	closeModal()
+})
+// Listen button Hold
+hold.addEventListener('click', () => {
+	holdMp3.play()
+	addPointTOGlobalPoint()
+	verif()
+	sayIdentite()
+	removeOnline()
+})
+
+buttonModalWin.addEventListener('click', closeModalWin)
+// Listen for open click
+newGame.addEventListener('click', openModal);
+
+rules.addEventListener('click', openModal2)
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+closeBtn2.addEventListener('click', closeModal2)
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+window.addEventListener('click', outsideClick2);
+
+function sayWinnerName1() {
+	winner.innerText = namePlayer1.innerText
+}
+
+function sayWinnerName2() {
+	winner.innerText = namePlayer2.innerText
+}
+
+
+let french = document.getElementById('french')
+let textLanguageEn = document.getElementsByClassName('textLanguageEn')
+let textLanguageFr = document.getElementsByClassName('textLanguageFr')
+let english = document.getElementById('english')
+
+
+french.addEventListener('click', () => {
+	for (let i = 0; i < textLanguageEn.length; i++) {
+		textLanguageEn[i].style.display = 'none';
+		textLanguageFr[i].style.display = 'block';
+	}
+
+})
+
+english.addEventListener('click', () => {
+	for (let i = 0; i < textLanguageEn.length; i++) {
+		textLanguageEn[i].style.display = 'block';
+		textLanguageFr[i].style.display = 'none';
+	}
+})
